ffbddc5a24d2fcb561aab312de383ca06efc6ced
tophat8855 <hrfarley@gmail.com>
2015-01-12 15:15:18 -0800

    check if number is even or odd

---
+++ evenodd.js
@@ -0,0 +1,9 @@
+//check if number is even or odd and report
+for(var i = 0; i <= 20; i++){
+  if (i % 2 === 0) {
+    console.log(i + " is even")
+  }
+  else {
+    console.log(i + " is odd")
+  }
+}
